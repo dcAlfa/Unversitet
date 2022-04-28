f8ce47ef7ed39270030a9818dbc77027ad565427
dcAlfa <muhammadjonov620@gmail.com>
2022-04-28 23:09:57 +0500

    Deyarli bitdi

--- App/views.py
+++ App/views.py
@@ -4,6 +4,18 @@ from .models import *
 def asosiy(request):
     return render(request, "asosiy.html")
 def ustoz(request):
+    if request.method=="POST":
+        if request.POST.get('n')== "False":
+            natija = False
+        else:
+            natija = True
+        Ustoz.objects.create(
+            ism =  request.POST.get('ismi'),
+            yosh = request.POST.get('y'),
+            nafaqada = natija,
+            sana = request.POST.get('v')
+        )
+        return redirect("/ustozlar/")
     soz = request.GET.get("qidirish")
     if soz==None or soz=="":
         m = Ustoz.objects.filter().order_by('ism')
@@ -13,6 +25,18 @@ def ustoz(request):
 
 
 def fan(request):
+    if request.method=="POST":
+        if request.POST.get('a')==False:
+            natija = False
+        else:
+            natija = True
+        Fan.objects.create(
+            nom = request.POST.get('n'),
+            cod = request.POST.get('c'),
+            vaqt = request.POST.get('v'),
+            is_active = natija,
+        )
+        return redirect("/fanlar/")
     soz = request.GET.get("qidirish")
     if soz==None or soz=="":
         m = Fan.objects.filter().order_by('nom')
@@ -20,12 +44,25 @@ def fan(request):
         m = Fan.objects.filter(nom=soz)
     return render(request, "Fan.html", {'Fanlar':m})
 def yonalish(request):
+    a = Ustoz.objects.all()
+    b = Fan.objects.all()
+    if request.method=="POST":
+        d = request.POST.get('ustozi')
+        g = request.POST.get('fani')
+        ustozlari = Ustoz.objects.get(id=d)
+        fanlari = Fan.objects.get(id=g)
+        Yonalish.objects.create(
+            nom = request.POST.get("n"),
+            fanlar = fanlari,
+            ustozlar = ustozlari
+        )
+        return redirect("/birlashma/")
     soz = request.GET.get("qidirish")
     if soz==None or soz=="":
         hammasi = Yonalish.objects.filter().order_by('nom')
     else:
         hammasi = Yonalish.objects.filter(nom=soz)
-    return render(request, "Yonalish.html", {'Hammasi': hammasi})
+    return render(request, "Yonalish.html", {'Hammasi': hammasi, "shablon": a, "fanlar": b})
 
 def ochir_ustoz(request, son):
     Ustoz.objects.get(id=son).delete()
